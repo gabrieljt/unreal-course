c67ae6397073eca9ca2210296d24b19659f92c74
Gabriel J. Trabasso <gabrieljt@gmail.com>
2016-03-14 23:58:32 -0300

    Indent

--- section-2/BullsAndCows/Game/FBullCowGame.h
+++ section-2/BullsAndCows/Game/FBullCowGame.h
@@ -64,6 +64,6 @@ private:
 	int32									MyCurrentTry;
 	bool									bMyGuessedRight;
 	std::set<char>							MyGuessedLetters;
-	std::map <FString, EBullsAndCowsGuess>		MyGuessedWords;
+	std::map <FString, EBullsAndCowsGuess>	MyGuessedWords;
 	static const FString					MyIsogramWords[];
 };
