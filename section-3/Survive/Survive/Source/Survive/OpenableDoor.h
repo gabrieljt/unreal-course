a2a27250a57d44ab2a8dcd641c98fbec44e4b95d
Gabriel Trabasso <gabrieljt@gmail.com>
2016-04-26 00:45:12 -0300

    OpenableDoor BP; OnOpen and OnClose Requests; UE rocks!

--- section-3/Survive/Survive/Source/Survive/OpenableDoor.h
+++ section-3/Survive/Survive/Source/Survive/OpenableDoor.h
@@ -6,6 +6,7 @@
 #include "OpenableDoor.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnOpenRequest);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnCloseRequest);
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class SURVIVE_API UOpenableDoor : public UActorComponent
@@ -24,16 +25,14 @@ public:
 	UPROPERTY(BlueprintAssignable)
 		FOnOpenRequest OnOpenRequest;
 
+	UPROPERTY(BlueprintAssignable)
+		FOnCloseRequest OnCloseRequest;
+
 private:
 	void Close() const;
 
 	UPROPERTY(EditAnywhere)
 		float CloseDelay;
 
-	UPROPERTY(EditAnywhere)
-		float OpenAngle;
-
 	float LastOpenedTime;
-
-	float CloseAngle;
 };
